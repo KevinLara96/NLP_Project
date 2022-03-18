2b5f6b4443261942a12cb12f0f5fd586a03bf67b
KevinLara96 <l_kevin96@hotmail.com>
2022-03-17 22:21:43 -0600

    Final commit, finish details.

--- BPE.py
+++ BPE.py
@@ -1,30 +1,43 @@
 import collections
 
-from click import pass_obj
-
 pair_list = collections.defaultdict(int)
 
-def find_vocab(corpus):
+'''
+    Realiza el algoritmo BPE.
+    @param: corpus. Corpus en español.
+    @param: k. Número de iteraciones.
+'''
+def BPE(corpus, k): 
     global pair_list
     
+    #Si llegamos al número de iteraciones, se termina el algoritmo.
+    if(k==0):
+        return corpus
+
+    #Contamos el número de veces que aparece cada palabra en el corpus.
+    #es del tipo: {'c a d e n a' : Frecuencia}
     vocab = collections.Counter(corpus)
-    for i, freq in vocab.items():
-        char = i.split()
+    for i, freq in vocab.items(): #i->Palabra. freq->Número apariciones.
+        char = i.split() #Char es una lista que contiene las letras individuales.
         for j in range( len(char)-1 ):
-            pair_list[char[j], char[j+1]] += freq
+            pair_list[char[j], char[j+1]] += freq   #Se cuenta el número de apariciones de
+                                                    #los pares de letras
     
+
     max_ = 0
     letter = ()
+    #Se obtiene el par que más se repite.
     for pair, freq in pair_list.items():
         if(freq > max_):
             max_ = freq
             letter = pair
-    print(letter, max_)
+    #print(letter, max_)
 
+    #Se reemplaza el par, eliminando el espacio entre ambas letras.
     for i in range(100):
         for j in range(0, len(corpus[i])-2, 2):
             if(letter[0]==corpus[i][j] and letter[1]==corpus[i][j+2]):
-                print(letter[0], letter[1], ' -> ', corpus[i][j], corpus[i][j+2])
-                corpus[i] = corpus[i][0:j+1:] + corpus[i][j+2::]
-                print(corpus[i])
-        print()
+                corpus[i] = corpus[i][0:j+1:] + corpus[i][j+2::]    #Se elimina el espacio, creando una
+                                                                    #creando una nueva cadena.
+
+    return BPE(corpus, k-1) #Se repite el proceso.
--- Corpus.py
+++ Corpus.py
@@ -2,11 +2,12 @@ import string
 import nltk
 from nltk.corpus import cess_esp #Corpus español
 from nltk.corpus import stopwords
-#nltk.download('stopwords')
-#nltk.download('cess_esp')
+nltk.download('stopwords')
+nltk.download('cess_esp')
 
-import BPE
+import BPE #Se importa el módulo del algoritmo.
 
+#Pasamos el corpus a minúsculas.
 corpus = [i.lower() for i in cess_esp.words()]
 
 tokens = len(corpus)
@@ -14,14 +15,22 @@ tipos = len(set(corpus))
 print('Número de tókens: ', tokens)
 print('Número de tipos: ', tipos)
 
+
+#Se eliminan caracteres y cadenas que no sean puramente letras.
+#e.g. signos de exclamación, interrogación, etc.
 clean_corpus = [i for i in corpus if i.isalpha()]
 
+#Se eliminan las stopwords en español del corpus.
 stopwords = set(stopwords.words('spanish'))
-print()
-
 clean_corpus = [i for i in clean_corpus if i not in stopwords]
+
+
+#Se separan las palabras en caracteres separados por espacios.
 clean_corpus = [' '.join(list(x)) for x in clean_corpus]
 
 
-BPE.find_vocab(clean_corpus[:100])
-#BPE.BPE(clean_corpus, 5)
+clean_corpus = BPE.BPE(clean_corpus[:100], 20)
+for i in range(len(clean_corpus)):
+    print(clean_corpus[i])
+#Descomentar para imprimir el corpus actualizado.
+#print(clean_corpus)
